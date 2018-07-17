adf756e114361ca3c363e3737f9093d26b7fae6c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-16 17:48:06 -0700

    finshedmy forst swing gui and photo quiz and halfway hrough drumkit

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,34 +15,38 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+		JFrame frame=new JFrame();
 		//2. Set your JFrame object to be visible
-
+		frame.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
-		
+		// yes i do
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		//JFrame.EXIT_ON_CLOSE();
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel panel=new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel lable=new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
-		//8. Add the JPanel object to the JFrame
-
+		lable.setText("hallo");
+		//8. Add .set JPanel object to the JFrame
+		frame.add(panel);
 		//9. Add the JLabel object to the JPanel
-
+		panel.add(lable);
 		//10. Pack your JFrame.
-
+		frame.pack();
 		//11. Run your program again. Do you see your message.
-		
+		//yeeeeeeeeeeeeeet
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+		lable.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
-		//14. Run the program one more time. Do you see the image?
+		frame.pack();		//14. Run the program one more time. Do you see the image?
 	}
 	
+	private static void pack() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static ImageIcon loadImage(){
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
