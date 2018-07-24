03f30760be19efa84116d76dba91a5bf1efbb55d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-23 17:52:19 -0700

    completed photo quiz and soooo close to completeing drumkit

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -19,26 +19,29 @@ public class PhotoQuiz {
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
-
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
-		// 2. create a variable of type "Component" that will hold your image
-
-		// 3. use the "createImage()" method below to initialize your Component
-
-		// 4. add the image to the quiz window
-
-		// 5. call the pack() method on the quiz window
-
+                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the windowindow
+                // 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
+                String meme = "https://pleated-jeans.com/wp-content/uploads/2018/04/thumb-2-758x397.png";
+        		// 2. create a variable of type "Component" that will hold your image
+                Component comp= createImage(meme);
+        		// 3. use the "createImage()" method below to initialize your Component
+                
+        		// 4. add the image to the quiz window
+                quizWindow.add(comp);
+        // 5. call the pack() method on the quiz window
+                quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+               String ask=JOptionPane.showInputDialog("what does this meme relate to?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+                if (ask.equals("fortnite")) {
+					JOptionPane.showMessageDialog(null, "CORRRRRREEEEECCCCCTTTTT");
+                }
 		// 8. print "INCORRECT" if the answer is wrong
-
+                else {
+                JOptionPane.showMessageDialog(null, "nope");
+				}
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+                
 		// 10. find another image and create it (might take more than one line of code)
 
 		// 11. add the second image to the quiz window
