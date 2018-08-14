ed378cfd3dd1d6c68a77910af35ff2c087728ec4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-13 17:50:58 -0700

    finshed degrees converter and soooo close to finishing quiz answers

--- src/extra/degrees_coverter.java
+++ src/extra/degrees_coverter.java
@@ -1,12 +1,13 @@
 package extra;
 
-import java.awt.Button;
+import java.awt.JobAttributes;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -17,12 +18,15 @@ public class degrees_coverter implements ActionListener{
 	JTextField field=new JTextField(5);
 	JLabel lable1 =new JLabel();
 	JLabel lable2 =new JLabel();
+	int answer;
+	String number;
+	
+	
 	degrees_coverter(){	
 		frame.setVisible(true);
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500,500);
-		String num=field();
 		panel.add(field);
 		lable1.setText("degrees farhanheit");
 		panel.add(lable1);
@@ -30,19 +34,30 @@ public class degrees_coverter implements ActionListener{
 		panel.add(button);
 		panel.setVisible(true);
 		button.addActionListener(this);
+		field.getText();
+			
+		}
 		
+
 	
-	}
-private String field() {
+
 		// TODO Auto-generated method stub
-		return null;
-	}
+		
 public static void main(String[] args) {
 	degrees_coverter dc=new degrees_coverter();
 	
+		
 }
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
+	JButton button2= (JButton) arg0.getSource();
 	
-}}
+	if (button2==button) {
+	String far = field.getText();
+	int answer=Integer.parseInt(far);
+	int celsius= answer*5/9-32;
+	JOptionPane.showMessageDialog(null,celsius +" degrees celsius is "+ answer + " in farenheight" );
+	
+	}
+			}}
--- src/extra/quizansewers.java
+++ src/extra/quizansewers.java
@@ -2,35 +2,43 @@ package extra;
 
 public class quizansewers {
 
-//	1. food (string apples);
+	// 1. food (string apples);
 
-//void food(String fruit) {
-//    System.out.println("I like " + fruit);
-//}
-//
-//	2.truth(int x=3);
-	
-	
-	//void truth(int x) {
-  // if(x > 5)
-   //     System.out.println("Geeks rule");
-  //else
-    //     System.out.println("Nerds rule");
-//
-	//	}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-	
-	
-	
-	
-	
+	// void food(String fruit) {
+	// System.out.println("I like " + fruit);
+	// }
+	//
+	// 2.truth(int x=3);
+
+	// void truth(int x) {
+	// if(x > 5)
+	// System.out.println("Geeks rule");
+	// else
+	// System.out.println("Nerds rule");
+	//
+	// }
+	// void drawLine() {
+	// robot.move(100);
+	// robot.turn(90);
+	// robot.move(100);
+	// robot.move(100);
+	// robot.move(100);
+	// robot.turn(90);
+	// robot.move(100);
+	// robot.move(100);
+	//
+	//
+	//
+	//void jump(int height) {
+   // if (height > 100)
+     //   System.out.println("The mouse jumped over the moon.");
+  //if (height > 50)
+    //     System.out.println("The mouse jumped over the broom.");
+  //if (height > 5)
+    //     System.out.println("The mouse jumped over the candlestick.");
+}
+	//
+	//
+	//
+
 }
